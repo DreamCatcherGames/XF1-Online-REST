f4e6b487c82ccc83167b51e6f94f705c81c0bc4b
dcama <dcamachog1501@gmail.com>
2022-05-02 18:44:57 -0600

    Se desarrolla el Entity Framework, se agrega funcionalidad para realizar el login(esperando aprobacion)

---
+++ XFIA_REST/Controllers/AdminController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using XFIA_REST.Models;
+
+namespace XFIA_REST.Controllers
+{
+    public class AdminController : ApiController
+    {
+        MiscelaneousFunctions tools = new MiscelaneousFunctions();
+        /// <summary>
+        /// Endpoint for login requests from the administration login page
+        /// </summary>
+        /// <param name="admin"><see cref="Administrator"/> object that contains the credentials that need to be validated</param>
+        /// <returns><see cref="HttpStatusCode"/> object that contains the response and status code </returns>
+
+        [Route("api/Admin/loginRequest")]
+        public HttpResponseMessage Get([FromBody]Administrator admin)
+        {
+            if(tools.passwordVerifier(admin))
+            {
+                string token = tools.getToken();
+                tools.assignToken(admin, token);
+                return Request.CreateResponse(HttpStatusCode.OK,token);
+            }
+            return Request.CreateResponse(HttpStatusCode.Unauthorized, "Usuario o Contraseña incorrectos");
+        }
+
+        // GET: api/Admin/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Admin
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Admin/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Admin/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- XFIA_REST/Models/MiscelaneousFunctions.cs
+++ XFIA_REST/Models/MiscelaneousFunctions.cs
@@ -8,17 +8,26 @@ namespace XFIA_REST.Models
 {
     public class MiscelaneousFunctions
     {
+        private XF1OnlineEntities context;
         public MiscelaneousFunctions()
         {
-
+            this.context = new XF1OnlineEntities();
         }
-
+        /// <summary>
+        /// Generates a random token 
+        /// </summary>
+        /// <returns>A generated random token</returns>
         public string getToken()
         {
             string g = Convert.ToBase64String(Guid.NewGuid().ToByteArray());
             g = g.Replace("+", "");
             return g.Replace("/", "");
         }
+        /// <summary>
+        /// Encrypts a given <see cref="System.String"/> password using MD5 encryption method.
+        /// </summary>
+        /// <param name="password">The <see cref="System.String"/> password to encrypt</param>
+        /// <returns></returns>
         public List<string> passwordEncryptor(string password)
         {
             var rng = new System.Security.Cryptography.RNGCryptoServiceProvider();
@@ -31,6 +40,45 @@ namespace XFIA_REST.Models
             byte[] bytes = md5.ComputeHash(System.Text.Encoding.Unicode.GetBytes(password + salt));
             string result = BitConverter.ToString(bytes).Replace("-", String.Empty).ToLower();
             return new List<string>(new string[] { result, salt });
+        }
+        /// <summary>
+        /// Verifies if a given set of credentials are the same registered on the database
+        /// </summary>
+        /// <param name="user"><see cref="Administrator"> object that contains the credential atributes as password and username</param>
+        /// <returns><see cref="bool"> object that indicates if the credentials are valid</returns>
+        public bool passwordVerifier(Object user)
+        {
+            MD5 md5 = new MD5CryptoServiceProvider();
+            byte[] bytes;
+            string testPassword = "";
+            string storedPassword= "";
+            string storedSalt = "";
+            if(user is Administrator)
+            {
+                Administrator credentials = (Administrator)user;
+                Administrator testAdmin = context.Administrators.Find(credentials.Username);
+                if(testAdmin==null)
+                {
+                    return false;
+                }
+                testPassword = credentials.Password;
+                storedPassword = testAdmin.Password;
+                storedSalt = testAdmin.Salt;
+            }
+            
+            bytes = md5.ComputeHash(System.Text.Encoding.Unicode.GetBytes(testPassword + storedSalt));
+            string result = BitConverter.ToString(bytes).Replace("-", String.Empty).ToLower();
+
+            return result.Equals(storedPassword);
+        }
+        public void assignToken(Object user,string token)
+        {
+            if(user is Administrator)
+            {
+                context.Administrators.Find(((Administrator)user).Username).Token = token;
+                context.SaveChanges();
+            }
+
         }
     }
 }
