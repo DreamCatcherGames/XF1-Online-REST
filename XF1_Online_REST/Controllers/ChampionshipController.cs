2be8c347757ed8b84b29c1f11d9dfd2f79d163bb
dcama <dcamachog1501@gmail.com>
2022-05-08 23:35:04 -0600

    Se termina la funcionalidad de añadir campeonatos

---
+++ XF1_Online_REST/Controllers/ChampionshipController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using XF1_Online_REST.LogicScript;
+using XF1_Online_REST.LogicScripts;
+
+namespace XF1_Online_REST.Controllers
+{
+    public class ChampionshipController : ApiController
+    {
+        ChampionshipLogic logic = new ChampionshipLogic();
+
+        [Route("api/Championship")]
+        public string Get()
+        {
+            return "Oui";
+        }
+
+        // GET: api/Championship/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        /// <summary>
+        /// Controller that allows to POST new championships
+        /// </summary>
+        /// <param name="champ"></param>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        [Route("api/PostChampionship/{token}")]
+        public HttpResponseMessage Post([FromBody]Championship champ,string token)
+        {
+            return logic.championshipCreationRequest(champ,token);
+        }
+
+        // PUT: api/Championship/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Championship/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ XF1_Online_REST/LogicScript/ChampionshipLogic.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using XF1_Online_REST.LogicScripts;
+
+namespace XF1_Online_REST.LogicScript
+{
+    public class ChampionshipLogic
+    {
+        public XF1OnlineEntities dbContext;
+        public Tools tools;
+
+        public ChampionshipLogic()
+        {
+            dbContext = new XF1OnlineEntities();
+            tools = new Tools();
+        }
+
+        public HttpResponseMessage championshipCreationRequest(Championship champ,string token)
+        {
+            if (tools.verifyAdminToken(token))
+            {
+                if (tools.championshipDateVerifier(champ))
+                {
+                    if (tools.championshipTimeVerifier(champ))
+                    {
+                        champ.Unique_Key = tools.getChampionshipKey();
+                        champ.Public_League_Name = champ.Name + " Public League";
+                        champ.CurrentChamp = true;
+
+                        try
+                        {
+                            dbContext.Championships.First(o => o.CurrentChamp == true).CurrentChamp = false;
+                        }
+                        catch (Exception ex)
+                        {
+                            
+                        }
+                        dbContext.Championships.Add(champ);
+
+                        dbContext.SaveChangesAsync();
+
+                        return new HttpResponseMessage(HttpStatusCode.OK) { Content = new StringContent("Championship added successfully") };
+                    }
+                    return new HttpResponseMessage(HttpStatusCode.Conflict) { Content = new StringContent("The times provided for this championship are not correct") };
+                }
+                return new HttpResponseMessage(HttpStatusCode.Conflict) { Content = new StringContent("Dates provided bump into another championship dates or they are not correct") };
+            }
+            return new HttpResponseMessage(HttpStatusCode.Unauthorized) { Content = new StringContent("Invalid token") };
+        }
+
+    }
+}
